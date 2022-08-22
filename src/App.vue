060dc893d7ce74159bc58a9246141a651e1db96b
Runyasak Chaengnaimuang <world@odds.team>
2022-08-22 22:30:03 +0700

    feat: implement product cards with styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
+<script setup lang="ts">
+import { useFetch } from "@vueuse/core";
+import { Product } from "./models/product.model";
+
+const { data: products } = useFetch(
+  "https://fakestoreapi.com/products?limit=20"
+)
+  .get()
+  .json<Product[]>();
+</script>
+
 <template>
   <v-app>
     <v-main>
-      <v-app-bar color="secondary">
-        <v-app-bar-title class="text-center font-weight-bold text-h4">
+      <v-app-bar color="#15202B">
+        <v-app-bar-title
+          class="text-center font-weight-bold text-h4 text-white"
+        >
           MY SHOP
         </v-app-bar-title>
       </v-app-bar>
+      <v-container>
+        <v-row class="py-4">
+          <v-col v-for="product of products" :key="product.id" col="12" md="4">
+            <v-card class="mx-auto" :variant="'elevated'">
+              <v-img :src="product.image" height="200px"></v-img>
+
+              <v-card-title> {{ product.title }} </v-card-title>
+
+              <v-card-text> {{ product.price }} </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
