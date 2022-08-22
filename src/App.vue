3f80c4b7e11e7ca4c4ee9b24fb756c9ae4c36e9c
Runyasak Chaengnaimuang <world@odds.team>
2022-08-22 21:20:00 +0700

    feat: implement app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <v-btn>Hello World</v-btn>
+      <v-app-bar color="secondary">
+        <v-app-bar-title class="text-center font-weight-bold text-h4">
+          MY SHOP
+        </v-app-bar-title>
+      </v-app-bar>
     </v-main>
   </v-app>
 </template>
