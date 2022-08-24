a54db4677fa74c63b8b9b89663ad6ee1167e942f
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-08-24 21:28:19 +0700

    feat: implement product description on ProductDetailPage

--- src/pages/ProductDetail.vue
+++ src/pages/ProductDetail.vue
@@ -17,7 +17,20 @@ const { isFetching, data: product } = useFetch(
 
 <template>
   <v-container>
-    <v-img :src="product?.image" width="400"></v-img>
+    <div class="wrapper">
+      <div v-if="product" class="d-flex gap-12">
+        <v-img :src="product.image" width="400"></v-img>
+        <div class="product-description">
+          <span class="text-h4">{{ product.title }}</span>
+          <span>{{ product.description }}</span>
+
+          <div class="buttons-group">
+            <v-btn variant="text">buy now</v-btn>
+            <v-btn color="secondary"> add to cart </v-btn>
+          </div>
+        </div>
+      </div>
+    </div>
   </v-container>
   <v-overlay v-model="isFetching" class="align-center justify-center">
     <v-progress-circular
@@ -27,3 +40,29 @@ const { isFetching, data: product } = useFetch(
     ></v-progress-circular>
   </v-overlay>
 </template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  min-height: 660px;
+  height: calc(100vh - 6.5em);
+}
+
+.product-description {
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+}
+
+.gap-12 {
+  gap: 3rem;
+}
+
+.buttons-group {
+  margin-top: auto;
+  display: flex;
+  justify-content: flex-end;
+  gap: 2em;
+}
+</style>
